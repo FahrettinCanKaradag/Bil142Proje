3fd43394e7a3084566753ac84ffa0214ed6fb57b
FahrettinCanKaradag <146446716+FahrettinCanKaradag@users.noreply.github.com>
2024-03-18 14:43:44 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "GemiGenerator.h"
+
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+
+    std::srand(time(nullptr));
+
+    auto MyGemiGenerator=new GemiGenerator();
+
+
+
+
+
+
+
+
+
+
+}
